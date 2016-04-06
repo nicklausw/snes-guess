52e0dfd2f6a0af92a6bed473586d4e9a771c6a07
nicklausw <nick@berlin.com>
2016-04-06 16:55:54 -0400

    no need for macros.i

--- main.s
+++ main.s
@@ -103,9 +103,12 @@ zero_fill_byte:
 message:
   .byte "hello, snes! i am nicklausw!", $ff
 
+; palette macro
+.define bgr(cb, g, r) ((cb<<10)|(g<<5)|r)
+
 palette:
-  bgr 0, 0, 0
-  bgr 31, 31, 31
+  .word bgr(0, 0, 0)
+  .word bgr(31, 31, 31)
 palette_size:
 
 font:
